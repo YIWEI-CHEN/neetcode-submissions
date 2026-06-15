d84e560169d391401c4f943471a0c734286cce20
Yi-Wei Chen <feberium@gmail.com>
2026-06-15 13:59:38 -0700

    Add: design-tic-tac-toe - submission-1

---
+++ Data Structures & Algorithms/design-tic-tac-toe/submission-1.py
@@ -0,0 +1,32 @@
+class TicTacToe:
+
+    def __init__(self, n: int):
+        self.n = n
+        self.rows = [0] * n
+        self.cols = [0] * n
+        self.diagonal = 0
+        self.anti_diagonal = 0
+
+    def move(self, row: int, col: int, player: int) -> int:
+        delta = 1 if player == 1 else -1
+
+        self.rows[row] += delta
+        self.cols[col] += delta
+        if row == col:
+            self.diagonal += delta
+        if row + col == self.n - 1:
+            self.anti_diagonal += delta
+        
+        if (
+            abs(self.rows[row]) == self.n
+            or abs(self.cols[col]) == self.n
+            or abs(self.diagonal) == self.n
+            or abs(self.anti_diagonal) == self.n
+        ):
+            return player
+        return 0
+
+
+# Your TicTacToe object will be instantiated and called as such:
+# obj = TicTacToe(n)
+# param_1 = obj.move(row,col,player)
