42be93d3073e05b227fe6742e2713f57d2c90459
Yi-Wei Chen <feberium@gmail.com>
2026-07-14 12:03:40 -0700

    Add: word-pattern - submission-0

---
+++ Data Structures & Algorithms/word-pattern/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split(" ")
+        char_to_word = {}
+        word_to_char = {}
+
+        if len(words) != len(pattern):
+            return False
+
+        for c, w in zip(pattern, words):
+            if c in char_to_word and char_to_word[c] != w:
+                return False
+
+            if w in word_to_char and word_to_char[w] != c:
+                return False
+            
+            char_to_word[c] = w
+            word_to_char[w] = c
+        
+        return True
